bfdb1aa277ada0aee3bf6ab350e91fd9dd744560
acd02 <alex.acd02@gmail.com>
2020-08-16 10:03:06 +0200

    feat: add new animations

--- tailwind/animation.js
+++ tailwind/animation.js
@@ -2,6 +2,8 @@ module.exports = {
   heartbeat: 'heartbeat 5s cubic-bezier(0.455, 0.03, 0.515, 0.955) infinite both',
   'heartbeat-double-time':
     'heartbeat 1s cubic-bezier(0.455, 0.03, 0.515, 0.955) infinite both',
-  'fade-in': 'fade-in 0.15s cubic-bezier(0.390, 0.575, 0.565, 1.000) forwards',
-  'fade-out': 'fade-out 0.15s cubic-bezier(0.390, 0.575, 0.565, 1.000) forwards',
+  'fade-in-left': 'fade-in-left 0.25s cubic-bezier(0.390, 0.575, 0.565, 1.000) both',
+  'fade-in-right': 'fade-in-right 0.25s cubic-bezier(0.390, 0.575, 0.565, 1.000) both',
+  'fade-out-left': 'fade-out-left 0.25s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+  'fade-out-right': 'fade-out-right 0.25s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
 }
--- tailwind/keyframes.js
+++ tailwind/keyframes.js
@@ -10,16 +10,32 @@ module.exports = {
     '33%': { transform: 'scale(0.92)', 'animation-timing-function': 'ease-in' },
     '45%': { transform: 'scale(1)', 'animation-timing-function': 'ease-out' },
   },
-  'fade-in': {
+  'fade-in-left': {
     '0%': {
-      opacity: '0',
+      transform: 'translateX(-50px)',
+      opacity: 0,
     },
-    '100%': { opacity: '1' },
+    '100%': { transform: 'translateX(0px)', opacity: 1 },
   },
-  'fade-out': {
+  'fade-in-right': {
     '0%': {
-      opacity: '1',
+      transform: 'translateX(50px)',
+      opacity: 0,
     },
-    '100%': { opacity: '0' },
+    '100%': { transform: 'translateX(0px)', opacity: 1 },
+  },
+  'fade-out-left': {
+    '0%': {
+      transform: 'translateX(0px)',
+      opacity: 1,
+    },
+    '100%': { transform: 'translateX(-50px)', opacity: 0 },
+  },
+  'fade-out-right': {
+    '0%': {
+      transform: 'translateX(0px)',
+      opacity: 1,
+    },
+    '100%': { transform: 'translateX(50px)', opacity: 0 },
   },
 }
