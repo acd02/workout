dc820dc71233d03930bec2b7d13c808ded4317cc
acd02 <alex.acd02@gmail.com>
2020-08-30 19:26:14 +0200

    feat: improve stopwatch UI

--- src/components/organisms/Stopwatch/index.tsx
+++ src/components/organisms/Stopwatch/index.tsx
@@ -1,7 +1,10 @@
+import { when } from 'acd-utils'
 import cx from 'classcat'
-import React, { useEffect, useState } from 'react'
+import { useEffectAfterMount } from 'hooks/useEffectAfterMount'
+import React, { useEffect, useRef, useState } from 'react'
 
-import { toIntlNumberFormat } from './utils'
+import styles from './styles.module.css'
+import { setProgress, toIntlNumberFormat } from './utils'
 
 type Props = {
   limit?: number
@@ -9,16 +12,40 @@ type Props = {
 
 export function Stopwatch({ limit = 60 }: Props) {
   const [elapsedTime, setElapsedTime] = useState(0)
+  const circleRef = useRef<SVGCircleElement>(null)
+  const circumferenceRef = useRef(0)
 
   function incrementElapsedTime() {
     setElapsedTime(t => (t > 999 ? 0 : t + 1))
   }
 
   useEffect(() => {
+    when(circleRef.current).map(circle => {
+      const radius = circle.r.baseVal.value
+      circumferenceRef.current = radius * 2 * Math.PI
+
+      /* eslint-disable-next-line fp/no-mutation */
+      circle.style.strokeDasharray = `${circumferenceRef.current} ${circumferenceRef.current}`
+      /* eslint-disable-next-line fp/no-mutation */
+      circle.style.strokeDashoffset = `${circumferenceRef.current}`
+    })
+
     const intervalId = setInterval(incrementElapsedTime, 1000)
 
     return () => clearInterval(intervalId)
   }, [])
+
+  useEffectAfterMount({
+    func: () => {
+      elapsedTime <= limit &&
+        setProgress({
+          circle: circleRef.current,
+          circumference: circumferenceRef.current,
+          percent: (elapsedTime / limit) * 100,
+        })
+    },
+    deps: [elapsedTime],
+  })
 
   const formattedElapsedTime =
     limit < elapsedTime ? (
@@ -29,14 +56,36 @@ export function Stopwatch({ limit = 60 }: Props) {
       toIntlNumberFormat(elapsedTime)
     )
 
-  const classNames =
-    'tabular-nums border-solid border-4 border-gray-700 rounded-full w-40 h-40 flex flex-wrap items-center justify-center text-6xl transition-colors duration-300'
-
   return (
-    <div
-      className={cx([classNames, limit < elapsedTime && 'border-red-400 text-red-400'])}
-    >
-      {formattedElapsedTime}
+    <div className={styles.root}>
+      <svg
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMinYMin meet"
+        className={styles.svg}
+      >
+        <circle
+          ref={circleRef}
+          className={cx([
+            styles.circle,
+            elapsedTime > limit ? styles.strokeIsOverLimit : styles.stroke,
+          ])}
+          strokeWidth="2"
+          strokeLinecap="round"
+          fill="transparent"
+          r="48"
+          cx="50%"
+          cy="50%"
+        />
+      </svg>
+      <span
+        className={cx([
+          styles.text,
+          'tabular-nums text-6xl transition-colors duration-300',
+          limit < elapsedTime && 'text-red-400',
+        ])}
+      >
+        {formattedElapsedTime}
+      </span>
     </div>
   )
 }
--- src/components/organisms/Stopwatch/utils.ts
+++ src/components/organisms/Stopwatch/utils.ts
@@ -4,3 +4,15 @@ export function toIntlNumberFormat(value: number): string {
     minimumIntegerDigits: 2,
   }).format(value)
 }
+
+type SetProgressArgs = {
+  percent: number
+  circumference: number
+  circle: SVGCircleElement | null
+}
+
+export function setProgress({ percent, circumference, circle }: SetProgressArgs) {
+  const offset = circumference - (percent / 100) * circumference
+  /* eslint-disable-next-line fp/no-mutation */
+  if (circle) circle.style.strokeDashoffset = `${offset}`
+}
