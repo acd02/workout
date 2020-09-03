105172a8e12fb90c21e5cf1d954a161a012b460d
acd02 <alex.acd02@gmail.com>
2020-09-03 13:43:45 +0200

    fix: update jest config

--- jest.config.js
+++ jest.config.js
@@ -16,10 +16,14 @@ module.exports = {
   testMatch: ['<rootDir>/src/**/__tests__/*.spec.(js|ts|tsx)'],
   moduleFileExtensions: ['js', 'ts', 'tsx'],
   moduleDirectories: ['node_modules', 'jest/testUtils'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/',
-    "\\.(css|scss)$": "identity-obj-proxy"
-  }),
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths, {
+      prefix: '<rootDir>/',
+    }),
+    '\\.(css|scss)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|pdf|svg)$':
+      '<rootDir>/jest/moduleMapper/fileMock.ts',
+  },
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
