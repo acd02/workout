e2b6f107b9d42ab0349898483529872ddf77e847
acd02 <alex.acd02@gmail.com>
2021-04-02 11:51:58 +0200

    feat: set up "preact" config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 const computePath = require('./utils').computePath
+const withPrefresh = require('@prefresh/next')
 
-module.exports = {
-  webpack(config, options) {
+const config = {
+  experimental: {
+    modern: true,
+    polyfillsOptimization: true,
+  },
+
+  webpack(config, { dev, isServer }) {
     const esLintRule = {
       test: /\.tsx$/,
       enforce: 'pre',
       exclude: ['/node_modules/', '/.next/'],
-      loader: 'eslint-loader'
+      loader: 'eslint-loader',
     }
     config.module.rules.push(esLintRule)
 
@@ -20,9 +26,44 @@ module.exports = {
       pagesContent: computePath('./src/pagesContent/'),
       public: computePath('./public/'),
       theme: computePath('./src/theme.ts'),
-      utils: computePath('./src/utils')
+      utils: computePath('./src/utils'),
+    }
+
+    // Preact stuff
+    const splitChunks = config.optimization && config.optimization.splitChunks
+    if (splitChunks) {
+      const cacheGroups = splitChunks.cacheGroups
+      const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/
+      if (cacheGroups.framework) {
+        cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
+          test: preactModules,
+        })
+        cacheGroups.commons.name = 'framework'
+      } else {
+        cacheGroups.preact = {
+          name: 'commons',
+          chunks: 'all',
+          test: preactModules,
+        }
+      }
+    }
+
+    // Install webpack aliases:
+    const aliases = config.resolve.alias || (config.resolve.alias = {})
+    aliases.react = aliases['react-dom'] = 'preact/compat'
+
+    // inject Preact DevTools
+    if (dev && !isServer) {
+      const entry = config.entry
+      config.entry = () =>
+        entry().then(entries => {
+          entries['main.js'] = ['preact/debug'].concat(entries['main.js'] || [])
+          return entries
+        })
     }
 
     return config
-  }
+  },
 }
+
+module.exports = withPrefresh(config)
